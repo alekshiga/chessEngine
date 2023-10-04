11ab425401a1717de1dcc9e9374b0ba8768b52de
Sergey <alekshiga@vk.com>
2023-10-04 14:29:10 +0700

    Piece, board, move

--- source/repos/library/library/Header.h
+++ source/repos/library/library/Header.h
@@ -2,42 +2,217 @@
 #include <string>
 #include <vector>
 
-struct Animal {
+class Animal {
+private:
 	std::string name;
-	int weight;
-} AnimalExample;
+	std::string weight;
+	std::string kind;
+	std::string sex;
 
-struct House {
-	std::string adress;
-	std::string phoneNumber;
-} HouseExample;
+public:
+	Animal() = default;
+	Animal(std::string setName, std::string setKind, std::string setSex, std::string setWeight) {	//конструктор со всеми аргументами
+		name = setName;
+		weight = setWeight;
+		kind = setKind;
+		sex = setSex;
+	}
 
-struct Owner {
-	std::string name;
-	std::string birthDate;
-} OwnerExample;
+	Animal createAnimal() {
+		std::string nameStorage;
+		std::cout << "Введите кличку животного: " << std::endl;
+		std::cin >> nameStorage;
+		std::string kindStorage;
+		std::cout << "Укажите вид животного: " << std::endl;
+		std::cin >> kindStorage;
+		std::string sexStorage;
+		std::cout << "Укажите пол животного(м/ж): " << std::endl;
+		std::cin >> sexStorage;
+		std::string weightStorage;
+		std::cout << "Укажите вес животного, кг: " << std::endl;
+		std::cin >> weightStorage;
+		Animal newAnimal(nameStorage, kindStorage, sexStorage, weightStorage);
+		return newAnimal;
+	}
+	
+	std::string getWeight() const {
+		return weight;
+	}
 
-struct Pet {
-	struct Animal Animal;
-	struct House House;
-	struct Owner Owner;
+	std::string getName() const {
+		return name;
+	}
+	
+	std::string getKind() const {
+		return kind;
+	}
+
+	void setWeight(std::string newWeight) {
+		weight = newWeight;
+	}
+	void giveAnimalsInfo() const {
+		std::cout << "Кличка: " << name;
+		std::cout << "Вид: " << kind;
+		std::cout << "Пол: " << sex;
+		std::cout << "Вес, кг: " << weight;
+		std::cout << "--------------------------------------\n";
+	}
 };
 
-std::string PetFound(std::string name, std::vector<Pet> list) {
-	for (const auto& pet : list) {
-		if (pet.Animal.name == (name + "\n")) return "Номер хозяина: " + pet.House.phoneNumber;
+class House {
+private:
+	std::string adress;
+	std::string phoneNumber;
+	std::string howManyKidsLiving;
+public:
+	House() = default;             // конструктор по умолчанию
+	House(std::string setAdress, std::string setPhoneNumber, std::string setLivingKids) {	//конструктор со всеми аргументами
+		adress = setAdress;
+		phoneNumber = setPhoneNumber;
+		howManyKidsLiving = setLivingKids;
 	}
-	return "В списке нет питомца с такой кличкой";
-}
+	House createHouse() {
+		std::string adressStorage;
+		std::cout << "Введите адрес: " << std::endl;
+		std::cin >> adressStorage;
+		std::string phoneNumberStorage;
+		std::cout << "Укажите номер телефона: " << std::endl;
+		std::cin >> phoneNumberStorage;
+		std::string howManyKidsLivingStorage;
+		std::cout << "Укажите, сколько детей живут: " << std::endl;
+		std::cin >> howManyKidsLivingStorage;
+		House newHouse(adressStorage, phoneNumberStorage, howManyKidsLivingStorage);
+		return newHouse;
+	}
 
-void printInfo(std::vector<Pet> pets) {
-	std::cout << "Информация о питомцах\n-----------------------------------------------------" << std::endl;
-	for (auto & pet : pets) {
-		std::cout << "Кличка: " << pet.Animal.name;
-		std::cout << "Хозяин: " << pet.Owner.name;
-		std::cout << "Номер хозяина: " << pet.House.phoneNumber;
-		std::cout << "Адрес: " << pet.House.adress;
-		std::cout << "------------------------------------------------------\n";
+	std::string getPhoneNumber() const {
+		return phoneNumber;
 	}
-}
+
+	std::string getAdress() const {
+		return adress;
+	}
+	
+	std::string getKidsInfo() const {
+		return howManyKidsLiving;
+	}
+
+	void setAdress(std::string newAdress) {
+		adress = newAdress;
+	}
+
+	void setPhoneNumber(std::string newPhone) {
+		phoneNumber = newPhone;
+	}
+
+	void setKidsAmount(std::string newHowManyKidsLiving) {
+		howManyKidsLiving = newHowManyKidsLiving;
+	}
+
+	void giveHousesInfo() const {
+		std::cout << "Адрес: " << adress;
+		std::cout << "Номер телефона: " << phoneNumber;
+		std::cout << "Детей живет в этом доме: " << howManyKidsLiving;
+		std::cout << "--------------------------------------\n";
+	}
+};
+
+class Owner {
+private:
+	std::string name;
+	std::string birthday;
+	std::string responsibilityDegree;
+public:
+	Owner() = default;            // конструктор по умолчанию
+	Owner(std::string setName, std::string setBirthday, std::string setResponsibilityDegree) {	//конструктор со всеми аргументами
+		name = setName;
+		responsibilityDegree = setResponsibilityDegree;
+		birthday = setBirthday;
+	}
+	Owner createOwner() {
+		std::string nameStorage;
+		std::cout << "Введите ФИО: " << std::endl;
+		std::cin >> nameStorage;
+		std::string birthdayStorage;
+		std::cout << "Укажите дату рождения: " << std::endl;
+		std::cin >> birthdayStorage;
+		std::string degreeStorage;
+		std::cout << "Укажите степень ответственности хозяина: " << std::endl;
+		std::cin >> degreeStorage;
+		Owner newOwner(nameStorage, birthdayStorage, degreeStorage);
+		return newOwner;
+	}
+	std::string getName() const {
+		return name;
+	}
+
+	std::string getBirthday() const {
+		return birthday;
+	}
+
+	std::string getResponsibilityDegree() const {
+		return responsibilityDegree;
+	}
+
+	void giveOwnersInfo() const {
+		std::cout << "ФИО: " << name;
+		std::cout << "Дата рождения: " << birthday;
+		std::cout << "Степень ответственности: " << responsibilityDegree;
+		std::cout << "--------------------------------------\n";
+	}
+};
+
+class Residence {
+private: 
+	class House whereLiving;
+	class Owner whoLiving;
+public:
+	Residence() = default;
+	Residence(House setWhereLiving, Owner setWhoLiving) {
+		whereLiving = setWhereLiving;
+		whoLiving = setWhoLiving;
+	}
+
+	std::string getOwnerNameFromResidence() const {
+		return whoLiving.getName();
+	}
+
+	House getHouseFromResidence() const {
+		return whereLiving;
+	}
+
+	Owner getOwnerFromResidence() const {
+		return whoLiving;
+	}
+}; 
+
+
+class Pet {
+private:
+	class Animal AnimalInPet;
+	class House HouseInPet;
+	class Owner OwnerInPet;
+public:
+	Pet() = default;            // конструктор по умолчанию
+
+	Pet(Animal setAnimal, House setHouse, Owner setOwner) {      //конструктор со всеми аргументами
+		AnimalInPet = setAnimal;
+		HouseInPet = setHouse;
+		OwnerInPet = setOwner;
+	}
+
+	void givePetsInfo() {
+		std::cout << "\nКличка: " << AnimalInPet.getName();
+		std::cout << "Имя хозяина: " << OwnerInPet.getName();
+		std::cout << "Номер телефона: " << HouseInPet.getPhoneNumber() << std::endl;
+		std::cout << "--------------------------------\n";
+	}
+
+	void changePetOwner(Pet* changingPet, Owner newOwner) {
+		std::cout << "\nКличка: " << (*changingPet).AnimalInPet.getName();
+		std::cout << "Прежний хозяин: " << (*changingPet).OwnerInPet.getName();
+		(*changingPet).OwnerInPet = newOwner;
+		std::cout << "Новый хозяин: " << newOwner.getName() << std::endl;
+	}
+};
 
--- source/repos/library/library/library.cpp
+++ source/repos/library/library/library.cpp
@@ -2,18 +2,20 @@
 #include <iostream>
 #include "Header.h"
 #include <windows.h>
+#include <algorithm>
+#include <conio.h>
 
-std::vector<Pet> Pets;
 std::vector<Owner> Owners;
-std::vector<House> Houses;
 std::vector<Animal> Animals;
+std::vector<House> Houses;
+std::vector<Residence> Residences;
+std::vector<Pet> Pets;
 
-void appendPetsInfo(Owner petOwner, Animal animal, House house) {
-    Pet bufferPet;
-    bufferPet.Animal = animal;
-    bufferPet.House = house;
-    bufferPet.Owner = petOwner;
-    Pets.push_back(bufferPet);
+int findMinLength(std::vector<Owner> Owners, std::vector<Animal> Animals, std::vector<House> Houses) {
+    int min = Animals.size();
+    if (min > Owners.size()) min = Owners.size();
+    if (min > Houses.size()) min = Houses.size();
+    return min;
 }
 
 int main()
@@ -21,38 +23,125 @@ int main()
     setlocale(LC_ALL, "Rus");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    const char* name = "data.txt";
-    FILE* file = fopen(name, "r");
-    if (file == NULL) throw std::runtime_error("Файл не был открыт!");
-    else {
-        char* str = (char*)calloc(50, sizeof(char));
-        fgets(str, 50, file);
-        int count = int(*str) - 48;
-        for (int i = 0; i < count; ++i) {
-            Owner BufferPerson;
-            fgets(str, 50, file);
-            BufferPerson.name = str;
-            fgets(str, 50, file);
-            BufferPerson.birthDate = str;
-            Owners.push_back(BufferPerson);
-            House BufferHome;
-            fgets(str, 50, file);
-            BufferHome.adress = str;
-            fgets(str, 50, file);
-            BufferHome.phoneNumber = str;
-            Houses.push_back(BufferHome);
-            Animal BufferAnimal;
-            fgets(str, 50, file);
-            BufferAnimal.name = str;
-            fgets(str, 50, file);
-            BufferAnimal.weight = atoi(str);
-            Animals.push_back(BufferAnimal);
-            appendPetsInfo(BufferPerson, BufferAnimal, BufferHome);
+    const char* animalsfile = "animals.txt";
+    const char* ownersfile = "owners.txt";
+    const char* housesfile = "houses.txt";
+    FILE* animalsstream = fopen(animalsfile, "r");
+    FILE* ownersstream = fopen(ownersfile, "r");
+    FILE* housesstream = fopen(housesfile, "r");
+    if (animalsstream == NULL || ownersstream == NULL || housesstream == NULL) throw std::runtime_error("File Open Error");
+    char* str = (char*)calloc(50, sizeof(char));
+    
+    // заполняем векторы животных, хозяев, домов из соответствующих файлов
+
+    for (int i = 0; fgets(str, 30, ownersstream) != NULL; ++i) {
+        std::string ownerName = str;
+        fgets(str, 15, ownersstream);
+        std::string birthday = str;
+        fgets(str, 3, ownersstream);
+        std::string responsibilityDegree = str;
+        Owner createOwnerRecord(ownerName, birthday, responsibilityDegree);
+        Owners.push_back(createOwnerRecord);
+    }
+    fclose(ownersstream);
+
+    for (int i = 0; fgets(str, 50, housesstream) != NULL; ++i) {
+        std::string houseAdress = str;
+        fgets(str, 15, housesstream);
+        std::string phoneNumber = str;
+        fgets(str, 3, housesstream);
+        std::string howManyKidsLiving = str;
+        House createHouseRecord(houseAdress, phoneNumber, howManyKidsLiving);
+        Houses.push_back(createHouseRecord);
+    }
+    fclose(housesstream);
+
+    for (int i = 0; fgets(str, 20, animalsstream) != NULL; ++i) {
+        std::string petName = str;
+        fgets(str, 20, animalsstream);
+        std::string petKind = str;
+        fgets(str, 3, animalsstream);
+        std::string petSex = str;
+        fgets(str, 5, animalsstream);
+        std::string petWeight = str;
+        Animal createAnimalRecord(petName, petKind, petSex, petWeight);
+        Animals.push_back(createAnimalRecord);
+    }
+    fclose(animalsstream);
+
+    int userChoiceOfOwner;
+
+    // связываем дом и жителя в класс резиденция
+
+    for (int i = 0; i < Owners.size(); i++) {
+        std::cout << "Укажите, по какому адресу живёт " << Owners[i].getName();
+        for (int j = 0; j < Houses.size(); j++) {
+            std::cout << j + 1 << ". " << Houses[j].getAdress();
         }
-        printInfo(Pets);        // функция выводит общую информацию о всех питомцах
-        std::string search;
-        std::cout << "\nВведите кличку животного, чтобы узнать номер его хозяина" << std::endl;
-        std::cin >> search;
-        std::cout << PetFound(search, Pets) << std::endl;   // функция позволяет по кличке узнать номер телефона хозяина(вдруг питомец потерялся и был найден кем-то)
+        std::cin >> userChoiceOfOwner;
+        Residence beforeAppendToVector(Houses[userChoiceOfOwner - 1], Owners[i]);
+        Residences.push_back(beforeAppendToVector);
+    }
+
+    std::cout << "Информация о всех хозяевах\n--------------------------------------\n";
+    for (int i = 0; i < Owners.size(); ++i) {
+        std::cout << i + 1 << ". ";
+        Owners[i].giveOwnersInfo();
+    }
+    std::cout << "\nИнформация о всех животных\n--------------------------------------\n";
+    for (int i = 0; i < Animals.size(); ++i) {
+        std::cout << i + 1 << ". ";
+        Animals[i].giveAnimalsInfo();
+    }
+    std::cout << "\nИнформация о всех домах\n--------------------------------------\n";
+    for (int i = 0; i < Houses.size(); ++i) {
+        std::cout << i + 1 << ". ";
+        Houses[i].giveHousesInfo();
+    }
+    int userChoiseOfAnimal;
+    std::cout << "\nУкажите номер хозяина из списка, который хочет найти себе питомца: ";
+    std::cin >> userChoiceOfOwner;
+    std::cout << "Укажите номер животного из списка: ";
+    std::cin >> userChoiseOfAnimal;
+    char f;
+
+    // создаем пару животное - резиденция
+
+    do {
+        std::cout << "\nСоздать пару " << Owners[userChoiceOfOwner - 1].getName() << "----------\n" << Animals[userChoiseOfAnimal - 1].getName() << Animals[userChoiseOfAnimal - 1].getKind();
+        std::cout << "Enter - да / Esc - нет\n" << std::endl;
+        f = _getch();
+    } while (f != 13 && f != 27);
+    if (f == 13) {
+        std::string findResidenceByName = Owners[userChoiceOfOwner - 1].getName();
+        for (int i = 0; i < Residences.size(); i++) {
+            if (findResidenceByName == Residences[i].getOwnerNameFromResidence()) {
+                Pet beforeAppendToVector(Animals[userChoiseOfAnimal - 1], Residences[i].getHouseFromResidence(), Residences[i].getOwnerFromResidence());
+                Pets.push_back(beforeAppendToVector);
+            }
+        }
+    }
+
+    // например, поменяем хозяина у какого-нибудь животного, у которого уже есть хозяин
+
+    int userChoiceOfPet;
+    for (int i = 0; i < Owners.size(); ++i) {
+        std::cout << i + 1 << ".";
+        Owners[i].giveOwnersInfo();
+    }
+
+    for (int i = 0; i < Pets.size(); ++i) {
+        std::cout << i + 1 << ".";
+        Pets[i].givePetsInfo();
+    }
+
+    std::cout << "Введите номер питомца, у которого хотите поменять хозяина: ";
+    std::cin >> userChoiceOfPet;
+    std::cout << "Введите номер нового хозяина: ";
+    std::cin >> userChoiceOfOwner;
+    Pets[0].changePetOwner(&(Pets[userChoiceOfPet - 1]), Owners[userChoiceOfOwner - 1]);
+    for (int i = 0; i < Pets.size(); ++i) {
+        std::cout << i + 1 << ".";
+        Pets[i].givePetsInfo();
     }
 }
