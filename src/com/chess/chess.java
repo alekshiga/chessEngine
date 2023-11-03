f9306bea68e0a5b1ef6e8c4240aa30bbc834f00c
Sergey <144024686+alekshiga@users.noreply.github.com>
2023-11-03 22:28:27 +0700

    Add files via upload

---
+++ src/com/chess/chess.java
@@ -0,0 +1,14 @@
+package com.chess;
+import com.chess.GUI.Table;
+
+import com.chess.engine.board.Board;
+
+public class chess {
+
+    public static void main(String[] args) {
+
+        Board board = Board.createStandartBoard();
+        Table table = new Table();
+    }
+
+}
