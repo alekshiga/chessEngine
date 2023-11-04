1ac60a584a1092b4aa0d6c77c92233ba49877f45
Sergey <144024686+alekshiga@users.noreply.github.com>
2023-11-05 00:59:53 +0700

    foolsmate test added
    A new test that checks the minimax algorithm on the example of foolsmate

--- src/com/chess/test/BoardTest.java
+++ src/com/chess/test/BoardTest.java
@@ -1,7 +1,14 @@
 package com.chess.test;
 
+import com.chess.engine.AI.Minimax;
+import com.chess.engine.AI.MoveStrategy;
 import com.chess.engine.board.Board;
 import com.chess.engine.*;
+import com.chess.engine.board.BoardBuilder;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.player.MoveStatus;
+import com.chess.engine.player.MoveTransition;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,4 +32,78 @@ class BoardTest {
         assertTrue(board.blackPlayer().toString().equals("Black"));
     }
 
+    @Test
+    public void testFoolsMate() {
+
+        final Board board = Board.createStandartBoard();
+
+        final MoveTransition firstWhite = board.currentPlayer().makeMove(Move.MoveFactory.createMove(board,
+                                                                                                     BoardUtils.getCoordinateAtPosition("f2"),
+                                                                                                     BoardUtils.getCoordinateAtPosition("f3")));
+        assertTrue(firstWhite.getMoveStatus().isDone());
+
+        final MoveTransition firstBlack = firstWhite.getTransitionBoard().currentPlayer().makeMove(Move.MoveFactory.createMove(firstWhite.getTransitionBoard(),
+                                                                                                   BoardUtils.getCoordinateAtPosition("e7"),
+                                                                                                   BoardUtils.getCoordinateAtPosition("e5")));
+
+        assertTrue(firstBlack.getMoveStatus().isDone());
+
+        final MoveTransition secondWhite = firstBlack.getTransitionBoard().currentPlayer().makeMove(Move.MoveFactory.createMove(firstBlack.getTransitionBoard(),
+                                                                                                    BoardUtils.getCoordinateAtPosition("g2"),
+                                                                                                    BoardUtils.getCoordinateAtPosition("g4")));
+
+        assertTrue(secondWhite.getMoveStatus().isDone());
+
+        final MoveStrategy strategy = new Minimax(4);
+
+        final Move bestMove = Move.MoveFactory.createMove(secondWhite.getTransitionBoard(),
+                BoardUtils.getCoordinateAtPosition("d8"), BoardUtils.getCoordinateAtPosition("h4"));
+
+        final Move AIMove = strategy.execute(secondWhite.getTransitionBoard());
+
+        assertEquals(AIMove, bestMove);
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
