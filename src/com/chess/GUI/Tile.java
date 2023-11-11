8fc64304b0a34986f6874b1378e4350a079e5f92
Sergey <144024686+alekshiga@users.noreply.github.com>
2023-11-11 14:30:54 +0700

    //TODO Table -> Singleton
    A single instance of Table to provide a global access to this instance for a computer or a human
    Also need to add settings menu to choose player

---
+++ src/com/chess/GUI/Tile.java
@@ -0,0 +1,88 @@
+package com.chess.engine.board;
+
+import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.chess.engine.board.BoardUtils.NUM_TILES;
+
+public abstract class Tile {
+
+    protected final int tileCoordinate;
+
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+        for (int i = 0; i < NUM_TILES; ++i) {
+            emptyTileMap.put(i, new EmptyTile(i));  // creating a Map of empty tiles, then return an Immutable copy of it
+        }                                           // so nobody can change out map
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
+    }
+
+    private Tile(final int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isOccupied();
+
+    public abstract Piece getPiece();
+
+    public int getTileCoordinate() {
+        return this.tileCoordinate;
+    }
+
+    public static final class EmptyTile extends Tile {
+
+        private EmptyTile(int coordinate) {
+            super(coordinate);
+        }
+
+        @Override
+        public String toString() {
+            return "-";
+        }
+
+        @Override
+        public boolean isOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends Tile {
+
+        private final Piece pieceOnTile;
+
+        private OccupiedTile(final int tileCoordinate, final Piece pieceOnTile) {
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public String toString() {
+            return getPiece().getPieceAlliance().isBlack() ? getPiece().toString().toLowerCase() :
+                    getPiece().toString();
+        }
+
+        @Override
+        public boolean isOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
+}
